8949d008b671e5f74ffe3ec2bcdec0e250f52f5b
Rishabh Jaiswal <rishabhjaiswal@Rishabhs-MacBook-Air.local>
2026-08-22 08:57:06 +0530

    added changse in zeroGpool cors

--- src/server.js
+++ src/server.js
@@ -94,6 +94,8 @@ const allowedOrigins = [
   'http://127.0.0.1:3000',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
+  'https://app.kult.games',
+  'https://www.app.kult.games'
 ];
 
 app.use((req, res, next) => {
